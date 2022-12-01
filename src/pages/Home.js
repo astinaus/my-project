eb810fc2807158c313725eff51b9174acea96fb7
astinaus <slaayers0@gmail.com>
2022-12-01 19:30:24 +0900

    insert bg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { Bg } from "./HomeElements";
 
 const Home = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "90vh",
-      }}
-    >
-      <h1>Home</h1>
-    </div>
+    <Bg>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "90vh",
+        }}
+      >
+        <h1>Home</h1>
+      </div>
+    </Bg>
   );
 };
 
